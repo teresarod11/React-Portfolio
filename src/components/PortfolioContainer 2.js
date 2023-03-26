39c92720adf103949b8cdbe11243683bf2c83dd2
teresarod11 <rodriguez49372@gmail.com>
2023-03-26 19:47:05 -0400

    updated

---
+++ src/components/PortfolioContainer 2.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import Navbar from './Navbar';
+// // import Header from './pages/Header';
+// import Footer from './pages/Footer';
+// import ContactMe from './pages/ContactMe';
+// import AboutMe from './pages/AboutMe';
+// import Portfolio from './pages/Portfolio';
+
+
+//   return (
+//     <div>
+//       <Navbar />
+//       <AboutMe />
+//       <Portfolio />
+//       <ContactMe />
+//       <Footer />
+//     </div>
+//   );
+// }
